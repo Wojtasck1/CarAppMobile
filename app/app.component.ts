349bd65b49d0c51f3b0ea0062d04de05101facdd
Wojciech Korban <wojtasck1@o2.pl>
2017-07-18 11:52:54 +0200

    dupa

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "my-app",
-  template: `
-    <ActionBar title="Kwant CarApp"></ActionBar>
-    <!-- Your UI components go here -->
-  `
+  templateUrl: "./app.component.html",
+  // template: `
+  //   <ActionBar title="Kwant CarApp"></ActionBar>
+  //   <!-- Your UI components go here -->
+  // `
 })
 export class AppComponent {
   // Your TypeScript logic goes here
